b526c274532e953c4dd070abebb87b9004e164d4
何朝晖 <hzh@hechaohuidemacbook-pro.local>
2018-02-11 11:48:06 +0800

    Initial Commit

---
+++ HHImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HHImagePicker
+//
+//  Created by 何朝晖 on 11/02/2018.
+//  Copyright © 2018 daHuiGe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
